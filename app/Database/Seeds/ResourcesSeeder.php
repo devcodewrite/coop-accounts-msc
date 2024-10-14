3ee0f2474cc68f829e5a345368c93cfe31303e15
Eric Mensah <ericmensah@codewrite.org>
2024-10-14 19:30:32 +0000

    initial commit

---
+++ app/Database/Seeds/ResourcesSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class ResourcesSeeder extends Seeder
+{
+    public function run()
+    {
+        $data = [
+            ['id' => 'resources', 'name' => 'Resources'],
+            ['id' => 'roles', 'name' => 'Roles'],
+            ['id' => 'permissions', 'name' => 'Permissions'],
+            ['id' => 'users', 'name' => 'Users'],
+            ['id' => 'groups', 'name' => 'Groups'],
+            ['id' => 'accounts', 'name' => 'Member Accounts'],
+            ['id' => 'associations', 'name' => 'Associations'],
+            ['id' => 'communities', 'name' => 'Communities'],
+            ['id' => 'districts', 'name' => 'Districts'],
+            ['id' => 'regions', 'name' => 'Regions'],
+            ['id' => 'offices', 'name' => 'Offices'],
+            ['id' => 'organizations', 'name' => 'Organizations'],
+            ['id' => 'passbooks', 'name' => 'Passbooks']
+        ];
+        //
+        $this->db->table('resources')->insertBatch($data);
+    }
+}
